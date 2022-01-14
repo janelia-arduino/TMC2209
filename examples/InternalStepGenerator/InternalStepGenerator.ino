d482c0c863c69b0a6650c2377a85b54c6b06d25f
Peter Polidoro <peter@polidoro.io>
2022-01-14 16:21:35 -0500

    Remove Streaming.h dependency

--- examples/InternalStepGenerator/InternalStepGenerator.ino
+++ examples/InternalStepGenerator/InternalStepGenerator.ino
@@ -1,14 +1,20 @@
 #include <Arduino.h>
-#include <Streaming.h>
 #include <TMC2209.h>
 
 HardwareSerial & serial_stream = Serial1;
 
 const long BAUD = 115200;
 
-const int LOOP_DELAY = 2000;
+const int LOOP_DELAY = 100;
 
-const int32_t velocity = 100;
+const int32_t VELOCITY_MIN = -1000;
+const int32_t VELOCITY_MAX = 1000;
+const uint8_t RUN_CURRENT_PERCENT = 20;
+const uint8_t HOLD_CURRENT_PERCENT = 10;
+const uint8_t HOLD_DELAY_PERCENT = 50;
+
+int32_t velocity_inc = 10;
+int32_t velocity = 0;
 
 // Instantiate TMC2209
 TMC2209 stepper_driver;
@@ -19,32 +25,31 @@ void setup()
   Serial.begin(BAUD);
 
   stepper_driver.setup(serial_stream,TMC2209::SERIAL_ADDRESS_0);
+  stepper_driver.setAllCurrentValues(RUN_CURRENT_PERCENT,HOLD_CURRENT_PERCENT,HOLD_DELAY_PERCENT);
+
+  if (stepper_driver.communicating())
+  {
+    Serial.print("Communicating with stepper driver!\n");
+    Serial.print("\n");
+  }
+  else
+  {
+    Serial.print("Not communicating with stepper driver!\n");
+    return;
+  }
 }
 
 void loop()
 {
-  if (stepper_driver.communicating())
-  {
-    Serial << "Communicating with stepper driver!\n";
-    Serial << "\n";
-  }
-  else
-  {
-    Serial << "Not communicating with stepper driver!\n";
-    return;
-  }
-
   stepper_driver.moveAtVelocity(velocity);
-  Serial << "moveAtVelocity(" << velocity << ")\n";
+  Serial.print("moveAtVelocity(");
+  Serial.print(velocity);
+  Serial.print(")\n");
   delay(LOOP_DELAY);
 
-  stepper_driver.moveAtVelocity(0);
-  Serial << "moveAtVelocity(" << 0 << ")\n";
-  delay(LOOP_DELAY);
-
-  stepper_driver.moveAtVelocity(-velocity);
-  Serial << "moveAtVelocity(" << -velocity << ")\n";
-  delay(LOOP_DELAY);
-
-  Serial << "\n";
+  if ((velocity == VELOCITY_MIN) || (velocity == VELOCITY_MAX))
+  {
+    velocity_inc = -velocity_inc;
+  }
+  velocity += velocity_inc;
 }
--- examples/TestConnection/TestConnection.ino
+++ examples/TestConnection/TestConnection.ino
@@ -1,10 +1,13 @@
 #include <Arduino.h>
-#include <Streaming.h>
 #include <TMC2209.h>
 
 HardwareSerial & serial_stream = Serial1;
 
 const long BAUD = 115200;
+
+const uint8_t RUN_CURRENT_PERCENT = 20;
+const uint8_t HOLD_CURRENT_PERCENT = 10;
+const uint8_t HOLD_DELAY_PERCENT = 50;
 
 const int LOOP_DELAY = 2000;
 
@@ -23,63 +26,111 @@ void loop()
 {
   if (stepper_driver.communicating())
   {
-    Serial << "Communicating with stepper driver!\n";
-    Serial << "\n";
+    Serial.print("Communicating with stepper driver!\n");
+    Serial.print("\n");
   }
   else
   {
-    Serial << "Not communicating with stepper driver!\n";
+    Serial.print("Not communicating with stepper driver!\n");
     return;
   }
 
   TMC2209::Status status = stepper_driver.getStatus();
-  Serial << "status.over_temperature_warning = " << status.over_temperature_warning << "\n";
-  Serial << "status.over_temperature_shutdown = " << status.over_temperature_shutdown << "\n";
-  Serial << "status.short_to_ground_a = " << status.short_to_ground_a << "\n";
-  Serial << "status.short_to_ground_b = " << status.short_to_ground_b << "\n";
-  Serial << "status.low_side_short_a = " << status.low_side_short_a << "\n";
-  Serial << "status.low_side_short_b = " << status.low_side_short_b << "\n";
-  Serial << "status.open_load_a = " << status.open_load_a << "\n";
-  Serial << "status.open_load_b = " << status.open_load_b << "\n";
-  Serial << "status.over_temperature_120c = " << status.over_temperature_120c << "\n";
-  Serial << "status.over_temperature_143c = " << status.over_temperature_143c << "\n";
-  Serial << "status.over_temperature_150c = " << status.over_temperature_150c << "\n";
-  Serial << "status.over_temperature_157c = " << status.over_temperature_157c << "\n";
-  Serial << "status.current_scaling = " << status.current_scaling << "\n";
-  Serial << "status.stealth_mode = " << status.stealth_mode << "\n";
-  Serial << "status.standstill = " << status.standstill << "\n";
-  Serial << "\n";
+  Serial.print("status.over_temperature_warning = ");
+  Serial.print(status.over_temperature_warning);
+  Serial.print("\n");
+  Serial.print("status.over_temperature_shutdown = ");
+  Serial.print(status.over_temperature_shutdown);
+  Serial.print("\n");
+  Serial.print("status.short_to_ground_a = ");
+  Serial.print(status.short_to_ground_a);
+  Serial.print("\n");
+  Serial.print("status.short_to_ground_b = ");
+  Serial.print(status.short_to_ground_b);
+  Serial.print("\n");
+  Serial.print("status.low_side_short_a = ");
+  Serial.print(status.low_side_short_a);
+  Serial.print("\n");
+  Serial.print("status.low_side_short_b = ");
+  Serial.print(status.low_side_short_b);
+  Serial.print("\n");
+  Serial.print("status.open_load_a = ");
+  Serial.print(status.open_load_a);
+  Serial.print("\n");
+  Serial.print("status.open_load_b = ");
+  Serial.print(status.open_load_b);
+  Serial.print("\n");
+  Serial.print("status.over_temperature_120c = ");
+  Serial.print(status.over_temperature_120c);
+  Serial.print("\n");
+  Serial.print("status.over_temperature_143c = ");
+  Serial.print(status.over_temperature_143c);
+  Serial.print("\n");
+  Serial.print("status.over_temperature_150c = ");
+  Serial.print(status.over_temperature_150c);
+  Serial.print("\n");
+  Serial.print("status.over_temperature_157c = ");
+  Serial.print(status.over_temperature_157c);
+  Serial.print("\n");
+  Serial.print("status.current_scaling = ");
+  Serial.print(status.current_scaling);
+  Serial.print("\n");
+  Serial.print("status.stealth_mode = ");
+  Serial.print(status.stealth_mode);
+  Serial.print("\n");
+  Serial.print("status.standstill = ");
+  Serial.print(status.standstill);
+  Serial.print("\n");
+  Serial.print("\n");
 
-  stepper_driver.setAllCurrentValues(100,50,50);
+  stepper_driver.setAllCurrentValues(RUN_CURRENT_PERCENT,HOLD_CURRENT_PERCENT,HOLD_DELAY_PERCENT);
 
   TMC2209::Settings settings = stepper_driver.getSettings();
-  Serial << "settings.microsteps_per_step = " << settings.microsteps_per_step << "\n";
-  Serial << "settings.inverse_motor_direction_enabled = " << settings.inverse_motor_direction_enabled << "\n";
-  Serial << "settings.spread_cycle_enabled = " << settings.spread_cycle_enabled << "\n";
-  Serial << "settings.zero_hold_current_mode = ";
+  Serial.print("settings.microsteps_per_step = ");
+  Serial.print(settings.microsteps_per_step);
+  Serial.print("\n");
+  Serial.print("settings.inverse_motor_direction_enabled = ");
+  Serial.print(settings.inverse_motor_direction_enabled);
+  Serial.print("\n");
+  Serial.print("settings.spread_cycle_enabled = ");
+  Serial.print(settings.spread_cycle_enabled);
+  Serial.print("\n");
+  Serial.print("settings.zero_hold_current_mode = ");
   switch (settings.zero_hold_current_mode)
   {
     case TMC2209::NORMAL:
-      Serial<< "normal\n";
+      Serial.print("normal\n");
       break;
     case TMC2209::FREEWHEELING:
-      Serial<< "freewheeling\n";
+      Serial.print("freewheeling\n");
       break;
     case TMC2209::STRONG_BRAKING:
-      Serial<< "strong_braking\n";
+      Serial.print("strong_braking\n");
       break;
     case TMC2209::BRAKING:
-      Serial<< "braking\n";
+      Serial.print("braking\n");
       break;
   }
-  Serial << "settings.irun = " << settings.irun << "\n";
-  Serial << "settings.ihold = " << settings.ihold << "\n";
-  Serial << "settings.iholddelay = " << settings.iholddelay << "\n";
-  Serial << "settings.automatic_current_scaling_enabled = " << settings.automatic_current_scaling_enabled << "\n";
-  Serial << "settings.pwm_offset = " << settings.pwm_offset << "\n";
-  Serial << "settings.pwm_gradient = " << settings.pwm_gradient << "\n";
-  Serial << "\n";
+  Serial.print("settings.irun = ");
+  Serial.print(settings.irun);
+  Serial.print("\n");
+  Serial.print("settings.ihold = ");
+  Serial.print(settings.ihold);
+  Serial.print("\n");
+  Serial.print("settings.iholddelay = ");
+  Serial.print(settings.iholddelay);
+  Serial.print("\n");
+  Serial.print("settings.automatic_current_scaling_enabled = ");
+  Serial.print(settings.automatic_current_scaling_enabled);
+  Serial.print("\n");
+  Serial.print("settings.pwm_offset = ");
+  Serial.print(settings.pwm_offset);
+  Serial.print("\n");
+  Serial.print("settings.pwm_gradient = ");
+  Serial.print(settings.pwm_gradient);
+  Serial.print("\n");
+  Serial.print("\n");
 
-  Serial << "\n";
+  Serial.print("\n");
   delay(LOOP_DELAY);
 }
